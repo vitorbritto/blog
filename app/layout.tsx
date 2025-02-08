fdb6ba0ca799fb16790ef9fcac7de6924545b0b2
Vitor Britto <vitor.britto@base39.com.br>
2025-02-08 09:47:21 -0300

    chore: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,56 @@
-import type { Metadata } from "next";
-import { JetBrains_Mono } from "next/font/google";
-import { Toaster } from "sonner";
-import { Header } from "@/components/Header";
-import { About } from "@/components/About";
-import { Footer } from "@/components/Footer";
-import "./globals.css";
+import { Suspense } from 'react'
+import type { Metadata } from 'next'
+import { JetBrains_Mono } from 'next/font/google'
+import { Toaster } from 'sonner'
+import { Header } from '@/components/Header'
+import { About } from '@/components/About'
+import { Footer } from '@/components/Footer'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
+import './globals.css'
 
-const jetbrains = JetBrains_Mono({ subsets: ["latin"] });
+const jetbrains = JetBrains_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Vitor Britto",
-  description: "Um blog sobre tecnologia e carreira. 🚀",
+  title: 'Vitor Britto',
+  description: 'Um blog sobre tecnologia e carreira. 🚀',
+  keywords: ['Vitor Britto', 'blog', 'tecnologia', 'carreira'],
+  metadataBase: new URL('https://vitorbritto.com'),
   icons: {
-    icon: "/favicon.png",
+    icon: '/favicon.png'
   },
-};
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://vitorbritto.com',
+    title: 'Vitor Britto',
+    description: 'Um blog sobre tecnologia e carreira. 🚀',
+    siteName: 'Vitor Britto',
+    images: [
+      {
+        url: '/preview.png',
+        width: 800,
+        height: 600,
+        alt: 'Vitor Britto Logo'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Vitor Britto',
+    description: 'Um blog sobre tecnologia e carreira. 🚀',
+    images: ['/preview.png']
+  }
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${jetbrains.className} bg-zinc-950 text-zinc-100 pt-12`}
-      >
+      <head>
+        <Suspense>
+          <GoogleAnalytics />
+        </Suspense>
+      </head>
+      <body className={`${jetbrains.className} bg-zinc-950 text-zinc-100 pt-12`}>
         <Header />
         {children}
         <About />
@@ -33,5 +58,5 @@ export default function RootLayout({
         <Toaster />
       </body>
     </html>
-  );
+  )
 }
---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+
+import Script from 'next/script'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    gtag: (command: string, target: string, params?: Record<string, string>) => void
+  }
+}
+
+const GA_MEASUREMENT_ID = 'G-TVYDZHTT4D'
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const url = pathname + searchParams.toString()
+
+      if (window.gtag) {
+        window.gtag('config', GA_MEASUREMENT_ID, {
+          page_path: url
+        })
+      }
+    }
+  }, [pathname, searchParams])
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `
+        }}
+      />
+    </>
+  )
+}
