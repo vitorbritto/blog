3dc9f6928e2cc99f82f12204292fee11b2c07679
Vitor Britto <vitor.britto@paketa.com.br>
2026-05-02 00:10:21 -0300

    chore: update social links

--- config/socialLinks.ts
+++ config/socialLinks.ts
@@ -1,6 +1,11 @@
-import { Github, Linkedin, Twitter } from 'lucide-react'
+import { CodeXml, Github, Linkedin, Twitter } from 'lucide-react'
 
 export const socialLinks = [
+  {
+    href: 'https://vitorbritto.dev',
+    icon: CodeXml,
+    label: 'vitorbritto.dev'
+  },
   {
     href: 'https://github.com/vitorbritto',
     icon: Github,
